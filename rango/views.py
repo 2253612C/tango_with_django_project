9cc19d13a90b006e844e7289cec55bca531b6462
David Canavan <2253612C@student.gla.ac.uk>
2022-01-27 17:12:28 +0000

    added hyperlinks and completed chapter 3 excercises

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request): #request is a httpResponse object
-    return  HttpResponse("Rango says hey there partner!") #string represents the content of the page to send back to the client requesting the view
+    return  HttpResponse("<a href='/rango/about/'>Link to the about page</a>") #string represents the content of the page to send back to the client requesting the view
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Go back to the rango page</a>") #href defines the url to link to, the text between > and </a> defines the text containing the link
