1b31b1b69af504cc333c391473c7213d67469f44
David Canavan <2253612C@student.gla.ac.uk>
2022-01-30 22:46:38 +0000

    finished chapter 4 excercises and completed test OK

--- rango/views.py
+++ rango/views.py
@@ -7,12 +7,14 @@ def index(request): #request is a httpResponse object
     #note the key boldmessage matches to {{boldmessage}} in the template 
     context_dict ={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
 
+    #return HttpResponse("Rango says hey there partner! <a href='/rango/'>About</a>") #href defines the url to link to, the text between > and </a> defines the text containing the link
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
      
     context_dict ={'boldmessage':'This tutorial has been put together by David Canavan'}
 
-    return render(request, 'rango/index.html',context=context_dict)
+    return render(request, 'rango/about.html',context=context_dict)
 
-    #return HttpResponse("<a href='/rango/'>Go back to the rango page</a>") #href defines the url to link to, the text between > and </a> defines the text containing the link
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>") #href defines the url to link to, the text between > and </a> defines the text containing the link
