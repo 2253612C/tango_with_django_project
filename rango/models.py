63fba6724141c788ab552daa3bed036fc0b3b784
David Canavan <2253612C@student.gla.ac.uk>
2022-02-10 18:21:35 +0000

    completed chap 7 excl. excercises

--- rango/models.py
+++ rango/models.py
@@ -29,3 +29,16 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        
+        return cleaned_data
+
+
+
