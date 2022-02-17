2fb0d90bbf25c257a4400a8a2efc36478c8758fe
David Canavan <2253612C@student.gla.ac.uk>
2022-02-17 15:33:11 +0000

    finished chapter 7 excercises

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,11 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 
+
+
 class Category(models.Model):
+    
+    field_length=128
     name=models.CharField(max_length=128,unique=True)
     views=models.IntegerField(default=0)
     likes=models.IntegerField(default=0)
@@ -20,25 +24,16 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    field_length=128
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title=models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-
     def __str__(self):
         return self.title
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-        # If url is not empty and doesn't start with 'http://',
-        # then prepend 'http://'.
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
-        
-        return cleaned_data
+    
 
 
 
